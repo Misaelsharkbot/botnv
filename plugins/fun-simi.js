f570d58116d76b7b9a0fc50ae73d67971f5db94a
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-05-18 21:18:49 -0500

    Update fun-simi.js

--- plugins/fun-simi.js
+++ plugins/fun-simi.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, {text}) => {
-  let api = await fetch(`https://simsimi.info/api/?text=${text}&lc=es`)
+  let api = await fetch(`https://api-sv2.simsimi.net/v2/?text=${text}&lc=es`)
   let res = await api.json()
   m.reply(res.success)
 }
